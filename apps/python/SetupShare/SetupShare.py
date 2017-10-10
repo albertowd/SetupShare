443bd20ba9190af46f341d3f65745fdad471232f
Alberto Dietrich <alberto+git@albertowd.com.br>
2017-10-10 17:20:18 -0300

    Updating version

--- apps/python/SetupShare/SetupShare.py
+++ apps/python/SetupShare/SetupShare.py
@@ -28,7 +28,7 @@ def acMain(ac_version):
     ac.setIconPosition(GUI.app_window, 0, -10000)
     ac.setSize(GUI.app_window, 400, 400)
 
-    lb_version = ac.addLabel(GUI.app_window, "v1.0.0")
+    lb_version = ac.addLabel(GUI.app_window, "v1.0.1")
     ac.setPosition(lb_version, 10, 3)
 
     GUI.bt_refresh = GUI.img_buttow("refresh")
