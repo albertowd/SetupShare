48f8692c396251d7ecf1ab904c43b1cb1d4334e3
Alberto Dietrich <alberto.wollmann+git@gmail.com>
2018-07-17 16:16:01 -0300

    Updating db.

--- server/api/upload.php
+++ server/api/upload.php
@@ -10,7 +10,6 @@ $ret = false;
  * Mandatory parameter.
  */
 $setup = json_decode(file_get_contents("php://input"));
-$steamId = intval(param("id", "0"));
 
 /**
  * Check integrity.
@@ -19,8 +18,6 @@ if (!$setup) {
     error_log(json_last_error() . ": " . json_last_error_msg());
     error_log(file_get_contents("php://input"));
     abortExecution(403, "Setup is not valid.");
-} elseif ($steamId == 0) {
-    abortExecution(403, "Missing SteamId.");
 }
 
 /**
@@ -38,7 +35,7 @@ if (DBConnection::connect()) {
     $sql = "SELECT id
               FROM setup
              WHERE car = ? AND `name` = ? AND steam_id = ? AND track = ?";
-    $stmt = DBConnection::prepare($sql, array($setup->car, $steamId, $setup->name, $setup->track));
+    $stmt = DBConnection::prepare($sql, array($setup->car, $setup->steam_id, $setup->name, $setup->track));
     if ($stmt && $row = $stmt->fetchObject()) {
         $id = $row->id;
     }
@@ -48,13 +45,13 @@ if (DBConnection::connect()) {
          * Old setups, let's update it.
          */
         $sql = "UPDATE setup
-                   SET ac_version = ?, ini = ?, sp = ?, `version` = `version` + 1, visibility = ?
+                   SET ac_version = ?, driver = ?, ini = ?, sp = ?, `version` = `version` + 1, visibility = ?
                  WHERE id = ?";
 
         /**
          * Execute the query.
          */
-        $stmt = DBConnection::prepare($sql, array($setup->ac_version, $setup->ini, $setup->sp, $setup->visibility, $id));
+        $stmt = DBConnection::prepare($sql, array($setup->ac_version, $setup->driver, $setup->ini, $setup->sp, $setup->visibility, $id));
         if ($stmt) {
             $ret = $stmt->rowCount() > 0 ? "$setup->name updated." : "$setup->name not updated.";
         }
@@ -65,7 +62,7 @@ if (DBConnection::connect()) {
         $sql = "INSERT INTO setup(ac_version, car, driver, ini, `name`, sp, steam_id, track, visibility)
                 VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
         $values = array($setup->ac_version, $setup->car, $setup->driver, $setup->ini,
-            $setup->name, $setup->sp, $steamId, $setup->track, $setup->visibility);
+            $setup->name, $setup->sp, $setup->steam_id, $setup->track, $setup->visibility);
         /**
          * Execute the query.
          */
